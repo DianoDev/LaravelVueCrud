c1335acc041909dae4682b67e0fb001c00e8c55f
Birianderson <adrianomellocolombo@gmail.com>
2025-01-20 14:41:31 -0400

    1.0.17

--- src/app/Providers/GenerateModuleServiceProvider.php
+++ src/app/Providers/GenerateModuleServiceProvider.php
@@ -18,13 +18,15 @@ class GenerateModuleServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__ . '/../resources/js/components' => resource_path('./js/components/laravel-vue-crud'),
+            __DIR__ . '/../resources/js/components' => resource_path('js/components/laravel-vue-crud'),
         ], 'laravel-vue-crud-components');
+
         $this->publishes([
-            __DIR__ . '/../app/View' => path('/app/View'),
+            __DIR__ . '/../app/View' => app_path('View'),
         ], 'laravel-view');
+
         $this->publishes([
-            __DIR__ . '/../resources/views/components' =>  resource_path('./views'),
+            __DIR__ . '/../resources/views/components' => resource_path('views'),
         ], 'laravel-blade');
     }
 }
